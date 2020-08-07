c676b6981fb9b52e2c784be289b777150520eb76
Lape15 <larape1993@gmail.com>
2020-08-07 01:54:03 +0100

    please work

--- scripts/index.js
+++ scripts/index.js
@@ -8,28 +8,35 @@ const texts = document.querySelectorAll(".text");
 const menuBtn = document.querySelector(".menu-btn");
 const hamburger = document.querySelector(".burger");
 let showMenu = false;
+const box1 = document.querySelector(".about");
+const box2 = document.querySelector(".services");
+const box3 = document.querySelector(".products");
+const box4 = document.querySelector(".partners");
+const box5 = document.querySelector(".team");
+const box6 = document.querySelector(".contact");
 
 const slideImage = (n) => {
   slides.forEach((slide, index) => {
-    slide.style.top = "-550px";
+    slide.classList.add("active-slide");
     dots.forEach((dot, index) => {
-      dot.classList.remove("active");
+      dot.classList.add("active");
     });
     texts.forEach((text, index) => {
-      text.style.opacity = "0";
-      text.style.transform = "translateY(70px)";
+      text.style.opacity = "100";
+      text.style.transform = "translateY(10px)";
     });
     btns.forEach((btn, index) => {
-      btn.style.transform = "translateY(40px)";
-      btn.style.opacity = "0";
+      btn.style.transform = "translateY(20px)";
+      btn.style.opacity = "100";
     });
   });
-  slides[n].style.top = "0";
+  slides[n].classList.remove("active-slide");
+
   dots[n].classList.add("active");
-  texts[n].style.opacity = "100";
-  texts[n].style.transform = "translateY(20px)";
+  texts[n].style.opacity = "0";
+  texts[n].style.transform = "translateY(10px)";
   btns[n].style.transform = "translateY(20px)";
-  btns[n].style.opacity = "100";
+  btns[n].style.opacity = "0";
 };
 
 prev.addEventListener("click", () => {
@@ -57,9 +64,23 @@ function toggleMenu() {
     hamburger.classList.add("open");
     showMenu = true;
     menuBtn.classList.add("right");
+    box1.classList.add("shrink");
+    box1.classList.remove("open");
+    box2.classList.add("shrink-one");
+    box3.classList.add("shrink-two");
+    box4.classList.add("shrink-three");
+    box5.classList.add("shrink-four");
+    box6.classList.add("shrink-five");
   } else {
     hamburger.classList.remove("open");
     menuBtn.classList.remove("right");
     showMenu = false;
+    box1.classList.remove("shrink");
+    box1.classList.add("open");
+    box2.classList.remove("shrink-one");
+    box3.classList.remove("shrink-two");
+    box4.classList.remove("shrink-three");
+    box5.classList.remove("shrink-four");
+    box6.classList.remove("shrink-five");
   }
 }
